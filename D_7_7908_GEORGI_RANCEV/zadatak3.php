5eb1a82f42efe96b84f6973f926c7b2838c13af5
rancevgeorgi <rancevgeorgi@gmail.com>
2020-12-16 14:43:39 +0100

    sedmi zadatak

---
+++ D_7_7908_GEORGI_RANCEV/zadatak3.php
@@ -0,0 +1,12 @@
+<?php
+    $p = 120;
+    $m = 80;
+    $k = 60;
+    echo "Pera treba da dobije"; 
+    echo " ";
+    echo $p - ($p + $m - $k) / 2;
+    echo "<br>";
+    echo "Mika treba da dobije";
+    echo " ";
+    echo $m - ($p + $m - $k) / 2;
+?>
